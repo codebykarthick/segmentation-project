3007c40cfa29a915df8c0875d400671805069b38
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-02-24 01:34:17 +0000

    Explored the dimensions of the image and modified preprocessing script. The images are much smaller in size now. Updated .gitignore and README.md a bit.

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ CV for sem 2 for MSc AI stream. Mini project constitutes for 50% of the final co
 ## Setup
 Create a virtual environment (ideally conda) and install the packages listed in the requirements.txt file for the successful execution of the proejct.
 
-## Dataset
-The dataset the project uses is not on the repo for performance reasons and has to be downloaded first. Run `data_setup.py` for the script to download this and unzip in the directory needed by the project files for execution.
+## Experiments
+Jupyter notebooks that give some extra background information on the design choices made for the scope of the project.
+
+## How to run and other info
+### Dataset
+The dataset, both original and processed, the project uses is not on the repo for performance reasons and has to be downloaded first. Run `data_setup.py` for the script to download this and unzip in the directory needed by the project files for execution.
+
+If you have access only to the original but not the processed, ensure that it is nested under `data/Dataset/TrainVal` and run `preprocess_augment.py` to generate the needed images for training and testing. It resizes and augments the training data but only resizes the test data for evaluation.
--- src/preprocess_augment.py
+++ src/preprocess_augment.py
@@ -3,6 +3,7 @@ from PIL import Image
 import shutil
 import torch
 from torchvision import transforms
+import torchvision.transforms.functional as F
 from util import logger
 
 log = logger.setup_logger()
@@ -34,37 +35,23 @@ processed_train_img_path, processed_train_mask_path = get_image_mask_paths(
 processed_test_img_path, processed_test_mask_path = get_image_mask_paths(
     processed_test_path)
 
-
-def find_max_image_size(image_folder, max_width=0, max_height=0):
-    """Finds the largest common width and height across all images."""
-    for file_name in os.listdir(image_folder):
-        if file_name.endswith((".jpg", ".png")):
-            image_path = os.path.join(image_folder, file_name)
-            with Image.open(image_path) as img:
-                width, height = img.size
-                max_width = max(max_width, width)
-                max_height = max(max_height, height)
-
-    return max_width, max_height
-
-
-# Find the best image_size for our training and testing
-max_width, max_height = find_max_image_size(original_train_img_path, 0, 0)
-max_width, max_height = find_max_image_size(
-    original_test_img_path, max_width, max_height)
-image_size = (max_height, max_width)
+"""
+Refer notebook experiment 1, that explores our dataset train and test images and masks.
+We found that 560x600 covers 99%ile of images. We will resize all images to this size.
+"""
+image_size = (560, 600)
 
 log.info(f"Image size after processing (HxW): {image_size}")
 
 
 def process(
         image_folder, mask_folder, output_img_folder, output_mask_folder,
-        base_transform, augmentation_transforms, is_test=False):
+        augmentation_transforms, is_test=False):
     """Actually processes the images and masks. Resizes and augments if not test, 
     only resizes if test."""
     for i, file_name in enumerate(os.listdir(image_folder), 1):
         log.info(
-            f"Processing image {i}/{len(os.listdir(image_folder))}: {file_name}")
+            f"Processing image {i}/{len(os.listdir(image_folder))}: {file_name}: {(i/len(os.listdir(image_folder))*100):.2f}%")
         if file_name.endswith(".jpg") or file_name.endswith(".png"):
             image_path = os.path.join(image_folder, file_name)
             mask_name = file_name.replace(".jpg", ".png")
@@ -72,38 +59,44 @@ def process(
 
             # Open images
             image = Image.open(image_path).convert("RGB")
-            mask = Image.open(mask_path).convert(
-                "RGB")
+            mask = Image.open(mask_path).convert("RGB")
 
-            # Resize & Save Original Images
-            resized_image = base_transform(image)
-            resized_mask = base_transform(mask)
+            # Resize both image and mask together
+            image = F.resize(
+                image, image_size, interpolation=transforms.InterpolationMode.BILINEAR)
+            mask = F.resize(
+                mask, image_size, interpolation=transforms.InterpolationMode.NEAREST)
 
-            resized_image_pil = transforms.ToPILImage()(resized_image)
-            resized_mask_pil = transforms.ToPILImage()(resized_mask)
+            # Save resized images
+            image.save(os.path.join(
+                output_img_folder, f"resized_{file_name}"), format="JPEG")
+            mask.save(os.path.join(
+                output_mask_folder, f"resized_{mask_name}"), format="PNG")
 
-            resized_image_pil.save(os.path.join(
-                output_img_folder, f"resized_{file_name}"))
-            resized_mask_pil.save(os.path.join(
-                output_mask_folder, f"resized_{mask_name}"))
+            # Random seed for consistency
+            seed = torch.randint(0, 10000, (1,)).item()
+            torch.manual_seed(seed)
 
             if not is_test:
                 # Augment images
                 for i, transform in enumerate(augmentation_transforms):
-                    # Random seed for consistency
-                    seed = torch.randint(0, 10000, (1,)).item()
-                    torch.manual_seed(seed)
-                    augmented_image = transform(image)
+                    # Get the same crops for image and mask
+                    if isinstance(transform.transforms[0], transforms.RandomRotation):
+                        # Get same random rotation angle
+                        angle = transforms.RandomRotation.get_params([-30, 30])
+                        augmented_image = F.rotate(image, angle)
+                        augmented_mask = F.rotate(mask, angle)
+                    elif isinstance(transform.transforms[0], transforms.ColorJitter):
+                        # Only apply color jitter to image
+                        augmented_image = transform(image)
+                        augmented_mask = mask
+                    else:
+                        augmented_image = transform(image)
+                        augmented_mask = transform(mask)
 
-                    torch.manual_seed(seed)
-                    augmented_mask = transform(mask)
-
-                    augmented_image_pil = transforms.ToPILImage()(augmented_image)
-                    augmented_mask_pil = transforms.ToPILImage()(augmented_mask)
-
-                    augmented_image_pil.save(os.path.join(
+                    augmented_image.save(os.path.join(
                         output_img_folder, f"augmented_{i}_{file_name}"))
-                    augmented_mask_pil.save(os.path.join(
+                    augmented_mask.save(os.path.join(
                         output_mask_folder, f"augmented_{i}_{mask_name}"))
     log.info(
         f"Processed {len(os.listdir(image_folder))} images in {image_folder}")
@@ -121,43 +114,29 @@ def preprocess_and_augment():
     os.makedirs(processed_test_img_path, exist_ok=True)
     os.makedirs(processed_test_mask_path, exist_ok=True)
 
-    # We should resize all the original images first.
-    base_transforms = transforms.Compose([
-        transforms.Resize(image_size),
-        transforms.ToTensor()
-    ])
-
     # Now separate augmentations to increase the dataset size
     augment_transforms = [
         transforms.Compose([
-            transforms.Resize(image_size),
             transforms.RandomHorizontalFlip(p=1.0),  # Flip
-            transforms.ToTensor(),
         ]),
         transforms.Compose([
-            transforms.Resize(image_size),
             transforms.RandomRotation(degrees=30),  # Rotate
-            transforms.ToTensor(),
         ]),
         transforms.Compose([
-            transforms.Resize(image_size),
             transforms.RandomResizedCrop(
                 image_size, scale=(0.8, 1.0)),  # Random Crop
-            transforms.ToTensor(),
         ]),
         transforms.Compose([
-            transforms.Resize(image_size),
             transforms.ColorJitter(
                 brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Color Jitter
-            transforms.ToTensor(),
         ]),
     ]
 
     # Process training & test sets
     process(original_train_img_path, original_train_mask_path, processed_train_img_path,
-            processed_train_mask_path, base_transforms, augment_transforms, False)
+            processed_train_mask_path, augment_transforms, False)
     process(original_test_img_path, original_test_mask_path, processed_test_img_path,
-            processed_test_mask_path, base_transforms, augment_transforms, True)
+            processed_test_mask_path, augment_transforms, True)
 
     log.info("Dataset preprocessing and augmentation completed!")
 
