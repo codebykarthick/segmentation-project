8f40feff3a9be40db128b8d4255ab1696d578dec
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-03-11 07:37:47 +0000

    Updated .gitignore to not track weights folder and updated scripts to run test evaluation, take a commit message and push changes to repo before shutting down.

--- src/scripts/windows/run_autoencoder_encode_train.bat
+++ src/scripts/windows/run_autoencoder_encode_train.bat
@@ -1,5 +1,18 @@
 cd ..\..
+set /p commit_message=Enter commit message: 
+
+if "%commit_message%"=="" (
+    echo Commit message cannot be empty. Exiting...
+    exit /b
+)
 
 python runner.py autoencoder train 10
+python runner.py autoencoder test
+
+cd ..
+
+git add .
+git commit -m "%commit_message%"
+git push origin main
 
 shutdown -s -f -t 0
--- src/scripts/windows/run_unet_train.bat
+++ src/scripts/windows/run_unet_train.bat
@@ -1,5 +1,18 @@
 cd ..\..
+set /p commit_message=Enter commit message: 
+
+if "%commit_message%"=="" (
+    echo Commit message cannot be empty. Exiting...
+    exit /b
+)
 
 python runner.py unet train 10
+python runner.py unet test
+
+cd ..
+
+git add .
+git commit -m "%commit_message%"
+git push origin main
 
 shutdown -s -f -t 0
