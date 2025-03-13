aed104bb4630b3d38c32a8ade86ca0ef53142f42
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-03-13 17:40:31 +0000

    Modified windows scripts to no longer push commits and run evaluation.

--- src/scripts/windows/run_auto_seg_train.bat
+++ src/scripts/windows/run_auto_seg_train.bat
@@ -1,18 +1,5 @@
 cd ..\..
-set /p commit_message=Enter commit message: 
-
-if "%commit_message%"=="" (
-    echo Commit message cannot be empty. Exiting...
-    exit /b
-)
 
 python runner.py autoencoder_segmentation train 10
-python runner.py autoencover_segmentation test
-
-cd ..
-
-git add .
-git commit -m "%commit_message%"
-git push origin main
 
 shutdown -s -f -t 0
--- src/scripts/windows/run_unet_train.bat
+++ src/scripts/windows/run_unet_train.bat
@@ -1,18 +1,5 @@
 cd ..\..
-set /p commit_message=Enter commit message: 
-
-if "%commit_message%"=="" (
-    echo Commit message cannot be empty. Exiting...
-    exit /b
-)
 
 python runner.py unet train 10
-python runner.py unet test
-
-cd ..
-
-git add .
-git commit -m "%commit_message%"
-git push origin main
 
 shutdown -s -f -t 0
