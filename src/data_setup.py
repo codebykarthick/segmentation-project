d32f46dbe6038852b48e695a61f28946d40236f3
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-03-28 13:26:36 +0000

    Modified data setup to download the 512x512 resize instead of the previous 256x256 resize and augmentations.

--- src/data_setup.py
+++ src/data_setup.py
@@ -5,8 +5,12 @@ import sys
 import zipfile
 
 DATASET_FILE_URL = "https://drive.google.com/file/d/1CIryq76zXU3ms0Rbpnf_WaL4S7_00fyC/view?usp=share_link"
+# Original resize 3000x1800 something bad
 # PROCESSED_URL = "https://drive.google.com/file/d/1eK8tYHxv-jtF1KN-B3M5YHSBdlwILZIz/view?usp=share_link"
-PROCESSED_URL = "https://drive.google.com/file/d/1UAQ8E-YOENu8K-kEkFkEl774hgH8-RiH/view?usp=sharing"
+# First proper - 256x256 better
+# PROCESSED_URL = "https://drive.google.com/file/d/1UAQ8E-YOENu8K-kEkFkEl774hgH8-RiH/view?usp=sharing"
+# 512x512 Resize
+PROCESSED_URL = "https://drive.google.com/file/d/1sVNF0qNFwxaCtkeaICmzhs6PfiaKrGTZ/view?usp=share_link"
 data_path = os.path.join(os.getcwd(), "data")
 processed_data_path = os.path.join(os.getcwd(), "data")
 original_file_path = os.path.join(data_path, "cv_dataset.zip")
