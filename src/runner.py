9800a9176c0a41cedc13a29d60ff05f9af682cea
Sri Hari Karthick <sriharikarthik2641999@gmail.com>
2025-04-04 16:35:18 +0100

    Added LR schedulers.

--- src/runner.py
+++ src/runner.py
@@ -41,6 +41,16 @@ class Runner:
         self.learning_rate = learning_rate
         self.optimizer = optim.Adam(
             self.model.parameters(), lr=self.learning_rate)
+        self.scheduler = torch.optim.lr_scheduler.SequentialLR(
+            self.optimizer,
+            schedulers=[
+                torch.optim.lr_scheduler.LinearLR(
+                    self.optimizer, start_factor=0.1, total_iters=5),
+                torch.optim.lr_scheduler.CosineAnnealingLR(
+                    self.optimizer, T_max=25)
+            ],
+            milestones=[5]
+        )
         self.patience = 3
         self.counter = 0
         self.batch_size = batch_size
@@ -107,11 +117,12 @@ class Runner:
 
                 epoch_loss += loss.item()
 
-                if (batch_idx + 1) % CONSTANTS["BATCH_LOG_FREQ"] == 0:
-                    tic = time()
-                    log.info(
-                        f"Epoch: [{epoch+1}/{num_epochs}], Batch: [{batch_idx+1}/{len(self.train_loader)}], Loss: {(epoch_loss/(batch_idx + 1)):.4f}, Time: {(tic - toc):.2f}s")
-                    toc = time()
+            if (batch_idx + 1) % CONSTANTS["BATCH_LOG_FREQ"] == 0:
+                tic = time()
+                log.info(
+                    f"Epoch: [{epoch+1}/{num_epochs}], Batch: [{batch_idx+1}/{len(self.train_loader)}], Loss: {(epoch_loss/(batch_idx + 1)):.4f}, Time: {(tic - toc):.2f}s")
+                toc = time()
+            self.scheduler.step()
 
             # Compute validation loss
             val_loss = self.validate()
@@ -169,11 +180,12 @@ class Runner:
 
                 epoch_loss += loss.item()
 
-                if (batch_idx + 1) % CONSTANTS["BATCH_LOG_FREQ"] == 0:
-                    tic = time()
-                    log.info(
-                        f"Epoch: [{epoch+1}/{num_epochs}], Batch: [{batch_idx+1}/{len(self.train_loader)}], Loss: {(epoch_loss / (batch_idx + 1)):.4f}, Time: {(tic - toc):.2f}s")
-                    toc = time()
+            if (batch_idx + 1) % CONSTANTS["BATCH_LOG_FREQ"] == 0:
+                tic = time()
+                log.info(
+                    f"Epoch: [{epoch+1}/{num_epochs}], Batch: [{batch_idx+1}/{len(self.train_loader)}], Loss: {(epoch_loss / (batch_idx + 1)):.4f}, Time: {(tic - toc):.2f}s")
+                toc = time()
+            self.scheduler.step()
 
             # Compute validation loss
             val_loss = self.validate()
